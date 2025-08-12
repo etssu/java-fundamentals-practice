7b059bcc3bb4b133e3fe802b28878cd5b6460419
etssu <121513071+etssu@users.noreply.github.com>
2025-08-12 08:26:23 +0200

    implement printing out

--- 03_language_familiess/src/Main.java
+++ 03_language_familiess/src/Main.java
@@ -3,7 +3,16 @@ import languages.*;
 public class Main {
     public static void main(String[] args) {
         Language german = new German();
+        Language slovak = new Slovak();
+        Language ukrainian = new Ukrainian();
+        Language swedish = new Swedish();
 
-        german.speak();
+        Language[] languages = {german, slovak, ukrainian, swedish};
+
+        for (Language language : languages) {
+            language.speak(); // overridden greetings
+            System.out.println(language.getInfo());
+            System.out.println();
+        }
     }
 }
