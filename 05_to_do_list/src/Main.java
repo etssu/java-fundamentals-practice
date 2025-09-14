a2da0e835cebd0e3edf9965ef4d8b9582a8d5e0d
etssu <121513071+etssu@users.noreply.github.com>
2025-09-14 08:41:14 +0200

    Refactor: move main logic into AppController

--- 05_to_do_list/src/Main.java
+++ 05_to_do_list/src/Main.java
@@ -1,45 +1,10 @@
 import java.util.Scanner;
-import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("=== To Do List ===\n1. Display all files.\n2. Open a file.\n3. Create a file.\n4. Delete a file.\n5. Quit.");
-            System.out.print("Enter your choice(a number): ");
-            int choice = sc.nextInt();
+        AppController ac = new AppController(sc);
 
-            ToDoService service = new ToDoService();
-            String fileName;
-            switch(choice){
-                case 1:
-                    service.getAllFiles();
-                    break;
-                case 2:
-                    System.out.print("Enter file name(without .ser): ");
-                    fileName = sc.nextLine(); // get file name
-
-                    service.openFile(fileName);
-                    break;
-                case 3:
-                    System.out.print("Enter file name(without .ser): ");
-                    fileName = sc.nextLine(); // get file name
-
-                    service.createFile(fileName);
-                    break;
-                case 4:
-                    System.out.print("Enter file name(without .ser): ");
-                    fileName = sc.nextLine();
-
-                    service.deleteFile(fileName);
-                    break;
-                case 5:
-                    System.out.println("Don't forget to come back later. Your tasks are waiting for you!");
-                    return;
-                default:
-                    System.out.println("Invalid Choice. Try again.");
-            }
-        }
     }
 }
